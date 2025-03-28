466b633ebea6ea958df77676a200d4000df2c43f
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-28 13:14:56 +0530

    Corrected code

--- Shreyansh VS Code/02_Conditionals/Question_2.py
+++ Shreyansh VS Code/02_Conditionals/Question_2.py
@@ -1,15 +1,15 @@
 import random
 name = input("Enter your name : ")
 age = int(input("Enter your age : "))
-day = random.choice(["Wednesday", "Thursday",])
+day = random.choice(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
 if age <= 17:
     price = 8
 else:
      price = 16
 if age <=17:
-    print(f"{name} is a Child hence he/she will pay {price}")
+    print(f"{name} is a Child hence he/she will pay ${price}")
 else:
-    print(f"{name} is a Adult hence he/she will pay {price}")
+    print(f"{name} is a Adult hence he/she will pay ${price}")
 if day == "Wednesday":
     discounted_price = price - 2
-    print(f"Today is {day}. So the ticket price is $2 off. Hence your price will be {discounted_price}" )
+    print(f"Today is {day}. So the ticket price is $2 off. Hence your price will be %{discounted_price}" )
--- Shreyansh VS Code/02_Conditionals/Question_3.py
+++ Shreyansh VS Code/02_Conditionals/Question_3.py
@@ -4,25 +4,17 @@ c=int(input("Enter English marks ="))
 d=int(input("Enter Science marks ="))
 e=int(input("Enter Social Science marks ="))
 p=(b+c+d+e)/4
+print("Your percentage =",p)
 if p >= 101:
         print ("Please enter valid marks")
         exit()
-print("Your percentage =",p)
-if p>=90:
-    print(a,"Grade is A1")
-elif p>=75:
-    print(a,"Grade is A2")
-elif p>=70:
-    print(a,"Grade is B1")
-elif p>=60:
-    print(a,"Grade is B2")
-elif p>=50:
-    print(a,"Grade is C1")
-elif p>=40:
-    print(a,"Grade is C2")
-elif p>=30:
-    print(a,"Grade is D1")
-elif p>=20:
-    print(a,"Grade is D2")
-else:
-    print(a,"Grade is E")
+if p>=100:
+    print(a,"Grade is A")
+elif p>=89:
+    print(a,"Grade is B")
+elif p>=79:
+    print(a,"Grade is C")
+elif p>=69:
+    print(a,"Grade is D")
+else :
+    print(a,"Grade is F")
