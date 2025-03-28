f02377203e93d5bd874fbc9772e5c19a57c4325f
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-28 14:17:57 +0530

    Completed a code on Weather Activity suggestion

---
+++ Shreyansh VS Code/02_Conditionals/Question_5.py
@@ -0,0 +1,8 @@
+import random
+weather = random.choice(["Sunny", "Rainy", "Snowy"])
+if weather == "Sunny":
+    print("It's a good day. Let's go out for a walk.")
+elif weather == "Rainy":
+    print("It's a rainy day. Let's stay at home and read a book.")
+else :
+    print("It's a snowy day. Let's make a snowman.")
