3a1ec05ed6ebc2323c9f027d98632f124cd03d7e
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-28 10:54:08 +0530

    organised files

---
+++ Shreyansh VS Code/01_basics/hello_python.py
@@ -0,0 +1,9 @@
+print("chai aur python")
+
+def chai (n):
+    print(n)
+
+chai("lemon tea")
+chai_one = "lemon tea"
+chai_two = "masala chai"
+chai_three = "coffee"   
---
+++ Shreyansh VS Code/Projects/List Duplicate.py
@@ -0,0 +1,3 @@
+list_1 = [10, 20, 60, 90, 20, 90]
+list_1 = list(set(list_1))
+print(list_1)
