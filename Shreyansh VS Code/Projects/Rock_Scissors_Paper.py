ffac67e2d71cf7cc88c1b9047305474796a5ce1d
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-28 14:11:39 +0530

    Made Rock Paper scissor game

---
+++ Shreyansh VS Code/Projects/Rock_Scissors_Paper.py
@@ -0,0 +1,23 @@
+import random
+player = input("Enter your choice (rock/paper/scissors): ")
+opponent = random.choice(['rock', 'paper', 'scissors'])
+print(f'Opponent choice is {opponent}')
+if player == opponent:
+    print("It's a tie!")
+elif player == 'rock':
+    if opponent == 'scissors':
+        print('You win!')
+    else:
+        print('You lose!')
+elif player == 'paper':
+    if opponent == 'rock':
+        print('You win!')
+    else:
+        print('You lose!')
+elif player == 'scissors':
+    if opponent == 'paper':
+        print('You win!')
+    else:
+        print('You lose!')
+else:
+    print('Please enter a valid move!')
