8cdf9eddc183be82fa7ae38604780a8232eb2165
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-28 11:19:13 +0530

    Deleted Unwanted Files

--- Shreyansh.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter your number: "))
-
-list_one = [5, 7, 1, 3, 8, 4, 9, 23, 86, 40]
-
-if a in list_one:
-    position = list_one.index(a)  
-    print(f"The value {a} is found at index {position}.")
-else:
-    print(f"The value {a} is not in the list.")
