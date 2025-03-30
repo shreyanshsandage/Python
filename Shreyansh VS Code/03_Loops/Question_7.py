2f5691c60c61f82daa589879056a80be7be10022
shreyanshsandage <shreyanshsandage@gmail.com>
2025-03-30 11:15:10 +0530

    Added input validation loop for numbers between 1 and 10

---
+++ Shreyansh VS Code/03_Loops/Question_7.py
@@ -0,0 +1,8 @@
+while True:
+        number = int(input("Enter value b/w 1 to 10: "))
+        if   1 <= number <= 10:
+                print("Valid number")
+                break
+        else:
+                print("Invalid number, try again")
+            
