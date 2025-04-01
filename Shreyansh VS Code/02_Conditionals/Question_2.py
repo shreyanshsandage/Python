ed0695f833416c243e130353409e507228c7904e
shreyanshsandage <shreyanshsandage@gmail.com>
2025-04-01 10:58:07 +0530

    Corrected code

--- Shreyansh VS Code/02_Conditionals/Question_2.py
+++ Shreyansh VS Code/02_Conditionals/Question_2.py
@@ -12,4 +12,4 @@ else:
     print(f"{name} is a Adult hence he/she will pay ${price}")
 if day == "Wednesday":
     discounted_price = price - 2
-    print(f"Today is {day}. So the ticket price is $2 off. Hence your price will be ${discounted_price}" )
+    print(f"Today is {day}. So the ticket price is 2$ less. Hence your price will be ${discounted_price}" )
