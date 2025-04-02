d08dec9622b2e40aa21d963a6270566cce4521bb
shreyanshsandage <shreyanshsandage@gmail.com>
2025-04-02 12:14:03 +0530

    Add sum_numbers function to calculate the sum of variable arguments

---
+++ Shreyansh VS Code/05_Functions/Question_7.py
@@ -0,0 +1,8 @@
+def sum_numbers(*args):
+    return sum(args)
+
+print(sum_numbers(1, 2, 3))         
+print(sum_numbers(10, 20, 30, 40))  
+print(sum_numbers())
+print(sum_numbers(5, 10, 15, 20, 25))
+print(sum_numbers(100, 200, 300, 400, 500, 600))
